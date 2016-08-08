03da157ea374ed8e8fefd025d970b6f1f1de7bd7
Torin Rudeen <torinmr@gmail.com>
2016-08-07 22:34:08 -0700

    Worked on correct angle rendering.

--- portal.cpp
+++ portal.cpp
@@ -3,6 +3,8 @@
 #include <glm/gtc/matrix_inverse.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
 
 #include "portal.h"
 #include "presentation.h"
@@ -49,7 +51,7 @@ void Portal::DrawStencil(GLuint shader, mat4 view, mat4 projection, GLuint bit) 
 
   mat4 preModel;
   // We bump it slightly off to avoid z-fighting.
-  preModel = translate(preModel, vec3(0.0f, 0.0f, -0.0001f));
+  preModel = translate(preModel, vec3(0.0f, 0.0f, -0.01f));
   preModel = scale(preModel, vec3(0.9f, 0.9f, 0.9f));
 
   DrawCommon(shader, preModel, view, projection);
@@ -93,7 +95,47 @@ void Portal::DrawCommon(GLuint shader, mat4 preModel, mat4 view, mat4 projection
 mat4 Portal::GetView(glm::vec3 incomingDirection) {
   glm::vec3 outgoingDirection;
   if (getPresentationStage() >= 2) {
+    // To calculate the correct view vector out of the linked portal, we use the
+    // following observation: if the portals we faced back to back, with their
+    // normals pointing in precisely opposite directions, then we would not need
+    // to modify the view direction at all. Thus, to calculate the correct
+    // outgoing view direction, we figure out what rotation would need to be
+    // applied to a hypothetical perfectly-aligned outgoing portal to transform
+    // it into the actual outgoing portal, and then apply this rotation to the
+    // view vector.
+    //
+    // Algorithm:
+    //
+    // Given incoming vector v1 and normal vectors n1 and n2 of the incoming and
+    // outgoing portals, we calculate outgoing vector v2 as follows:
+    //
+    // rotation_axis = -n1 cross n2
+    // rotation_angle = acos(-n1 dot n2)
+    // v2 = v1.rotate(rotation_axis, rotation_angle)
 
+    vec3 n1 = this->normal;
+    vec3 n2 = this->linked->normal;
+    vec3 v1 = incomingDirection;
+
+    cout << endl;
+    cout << "n1: " << n1.x << " " << n1.y << " " << n1.z << endl;
+    cout << "n2: " << n2.x << " " << n2.y << " " << n2.z << endl;
+
+    vec3 negative_n1 = -n1;
+    vec3 rotation_axis = normalize(cross(negative_n1, n2));
+    if (n1 == n2 || n1 == -n2) {
+      cout << "Special case" << endl;
+      vec3 up_rotation_axis = normalize(cross(normal, up));
+      rotation_axis = rotate(normal, 3.14159f /2.0f, up_rotation_axis);
+    }
+    cout << "rotation_axis: " << rotation_axis.x << " " << rotation_axis.y << " " << rotation_axis.z << endl;
+    float rotation_angle =
+      orientedAngle(negative_n1, n2, rotation_axis);
+    cout << "Rotation_angle: " << rotation_angle << endl;
+    cout << "v1: " << v1.x << " " << v1.y << " " << v1.z << endl;
+    vec3 v2 = rotate(v1, rotation_angle, rotation_axis);
+    cout << "v2: " << v2.x << " " << v2.y << " " << v2.z << endl;
+    outgoingDirection = v2;
   } else {
     outgoingDirection = linked->normal;
   }
@@ -110,10 +152,10 @@ void Portal::Setup() {
   }
 
   GLfloat vertices[] = {
-    -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-    -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-    0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-    0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+    -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+    -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+    1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+    1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
   };
   GLuint indices[] = {
     3, 1, 0,
